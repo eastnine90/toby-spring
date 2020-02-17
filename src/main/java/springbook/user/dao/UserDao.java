aeaad41b0346627af2da42e5e07489968b59f68e
django.franco <django.franco@kakaoenterprise.com>
2020-02-17 20:11:03 +0900

    DB 예외처리 부분 추가

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -57,10 +57,30 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        Connection c = dataSource.getConnection();
+        Connection c = null;
 
-        PreparedStatement ps = c.prepareStatement("delete from users");
-        ps.executeUpdate();
+        PreparedStatement ps = null;
+
+        try {
+            c = dataSource.getConnection();
+            ps = c.prepareStatement("delete from users");
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if(ps != null){
+                try{
+                    ps.close();
+                } catch(SQLException e){
+                }
+            }
+            if(c != null){
+                try{
+                    c.close();
+                } catch(SQLException e){
+                }
+            }
+        }
 
         ps.close();
         c.close();
